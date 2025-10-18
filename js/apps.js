7f8da63ffea2393db0ef5118ec3f091b46d37794
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-18 03:35:48 -0300

    html & css development commit

--- js/apps.js
+++ js/apps.js
@@ -1,23 +1,16 @@
+const rightElements = document.querySelector("img[class='rightElements']");
+const rightBottomElement = document.querySelector("img[class='rightBottomElement']");
+const sunElement = document.querySelector("img[class='sunElement']");
+const textMain = document.querySelector("h1");
+const formElement = document.querySelector("img[class='formulario']");
+let scrollPosition = window.scrollY;
 
-function validarLogin(event){
-    
-    event.preventDefault()
-    const userInput = document.getElementById("usuario")
-    const passInput = document.getElementById("senha")
-    const messageErro = document.querySelector("p[class='errorMessage']")
-    messageErro.textContent = ""
-    
-    const user = "admin"
-    const password = "12345"
-    
-    const usuario = userInput.value
-    const senha = passInput.value
-    
-    if(usuario === user & senha === password){
-        window.alert("Login efetuado com sucesso. Redirecioando")
-        return document.open("admin.html")
-        messageErro.textContent = ""
-    } else {
-        messageErro.textContent = "Credenciais Inválidas"
-    }
-}
+window.addEventListener('scroll', () => {
+    scrollPosition = window.scrollY;
+
+    rightElements.style.marginLeft = scrollPosition * 0.5 + 'px';
+    rightBottomElement.style.marginLeft = scrollPosition * 0.5 + 'px';
+    sunElement.style.marginLeft = scrollPosition * 0.5 + 'px';
+    textMain.style.marginTop = scrollPosition * 0.5 + 'px';
+    formElement.style.marginTop = scrollPosition * 0.3 + 'px';
+});
