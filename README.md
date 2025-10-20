9a494e682a440a290b23298d8337fc57c8f61c56
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-20 00:35:35 -0300

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# loginMedicina
+# Yuká Medicina
+
+> ### Projeto página de login + formulário (JovemTech)
+>**Professor:** Wallace Oliveira
+>
+>**Aluno:** Rodolfo Melo
+>
+>**Data:** outubro de 2025
+>
+> **Link:** https://dolfo-melo.github.io/loginMedicina/
+
+## Tecnologias
+
+> -> **HTML**
+>
+> -> **CSS**
+>
+> -> **Javascript**
+
+## Objetivos
+
+> -> Criar página **Login**
+>
+> -> Criar página **formulário**
+>
+> -> Cadastrar a informação do usuário e armazenar no **LocalStorage**
+>
+> -> Desenvolver **Front-End**
+
+## Aprendizados
+
+> -> Armazenar dados no LocalStorage para possível manipulação, mesmo após encerrar o navegador
+>
+> -> Organização corporativa do código
+>
+> -> Aplicar e melhorar a lógica de programação
+
+![yukaLogo](src/assets/yukaLogo.png)
+
+### Yuká Medicina by dolfo-melo © 2025
--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+// Parallax Effect
+    // Selecionando elementos do DOM
+    const rightElements = document.querySelector("img[class='rightElements']")
+    const leftElements = document.querySelector("img[class='leftElements']")
+    const sunElement = document.querySelector("img[class='sunElement']")
+    const textMain = document.querySelector("h1[class='mainTitle']")
+    const ajuMap = document.querySelector("img[class='mapaAju']")
+
+    // Pegando a posição inicial do scroll, usando let pois irá variar
+    let scrollPosition = window.scrollY
+
+    // Adicionando um evento de scroll na janela
+    window.addEventListener('scroll', () => {
+        scrollPosition = window.scrollY
+
+    // Movendo os elementos com base na posição do scroll
+        rightElements.style.left = scrollPosition * 0.5 + 'px'
+        leftElements.style.left = -scrollPosition * 0.5 + 'px'
+        sunElement.style.left = scrollPosition * 0.5 + 'px'
+        textMain.style.marginTop = scrollPosition * 0.5 + 'px'
+        })
+
 // Funcionalidade Gerais
     
     // Variáveis Globais
@@ -86,25 +108,3 @@
         listAgend.style.marginTop = "0"
         listText.textContent = listaAdmin.values
     }
-
-// Parallax Effect
-    // Selecionando elementos do DOM
-    const rightElements = document.querySelector("img[class='rightElements']")
-    const leftElements = document.querySelector("img[class='leftElements']")
-    const sunElement = document.querySelector("img[class='sunElement']")
-    const textMain = document.querySelector("h1[class='mainTitle']")
-    const ajuMap = document.querySelector("img[class='mapaAju']")
-
-    // Pegando a posição inicial do scroll, usando let pois irá variar
-    let scrollPosition = window.scrollY
-
-    // Adicionando um evento de scroll na janela
-    window.addEventListener('scroll', () => {
-        scrollPosition = window.scrollY
-
-    // Movendo os elementos com base na posição do scroll
-        rightElements.style.left = scrollPosition * 0.5 + 'px'
-        leftElements.style.left = -scrollPosition * 0.5 + 'px'
-        sunElement.style.left = scrollPosition * 0.5 + 'px'
-        textMain.style.marginTop = scrollPosition * 0.5 + 'px'
-        })
