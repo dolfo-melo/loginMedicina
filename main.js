0973f8d6222384685faec3fed3d187bb616be566
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-23 02:32:44 -0300

    Fix "style.css" and remove some Parallax Effects

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // Parallax Effect
     // Selecionando elementos do DOM
-    const rightElements = document.querySelector("img[class='rightElements']")
-    const leftElements = document.querySelector("img[class='leftElements']")
     const sunElement = document.querySelector("img[class='sunElement']")
     const textMain = document.querySelector("h1[class='mainTitle']")
     const ajuMap = document.querySelector("img[class='mapaAju']")
@@ -14,8 +12,6 @@
         scrollPosition = window.scrollY
 
     // Movendo os elementos com base na posição do scroll
-        rightElements.style.left = scrollPosition * 0.5 + 'px'
-        leftElements.style.left = -scrollPosition * 0.5 + 'px'
         sunElement.style.left = scrollPosition * 0.5 + 'px'
         textMain.style.marginTop = scrollPosition * 0.5 + 'px'
         })
